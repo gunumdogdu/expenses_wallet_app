089f34ad256ec6e3a03acf581791157d3d90c3b2
gunumdogdu <emirgunumdogdu@gmail.com>
2023-03-23 21:55:56 +0300

    Chart logic fix

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -29,15 +30,34 @@ class Chart extends StatelessWidget {
     );
   }
 
+  double get emirSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(16),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return Text('${data['day']}: ${data['amount']}');
-        }).toList(),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['day'] as String,
+                data['amount'] as double,
+                emirSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / emirSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
